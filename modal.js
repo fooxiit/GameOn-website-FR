fe0daeffa41cec1f6926e0bd4d80a9dc4ee6d387
guillian <guillian.edo@gmail.com>
2025-08-21 10:10:52 +0200

    feat add message on sucess sumbit

--- modal.js
+++ modal.js
@@ -10,13 +10,31 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBody = document.querySelector(".modal-body")
 const formData = document.querySelectorAll(".formData");
 const formHTML = document.querySelector("#form-reserve")
+const submitSucess = document.querySelector(".submit-sucess")
 
 const FirstNameFiels = document.querySelector("#firsname-field")
 const BirthDateFiels = document.querySelector("#birthdate-field")
 const LocationFiels = document.querySelector("#loction-field")
 const CguFiels = document.querySelector("#cgu-field")
+
+/**
+ * 
+ * @param {Element} e 
+ */
+function hidenElement(e){
+  e.classList.add("hidden")
+}
+
+/**
+ * 
+ * @param {Element} e 
+ */
+function showElement(e){
+  e.classList.remove("hidden")
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,13 +43,15 @@ formHTML.addEventListener("submit",submitForm)
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  showElement(modalbg)
 }
 /**
  * close modal
  */
 function closeModal() {
-  modalbg.style.display = null
+  hidenElement(modalbg)
+  hidenElement(submitSucess)
+  showElement(formHTML)
 }
 /**
  * validate form
@@ -86,7 +106,9 @@ function submitForm(e){
   e.preventDefault()
   const form = new FormData(e.target)
   if(!validateForm(form)) return
-
+  e.target.reset()
+  hidenElement(formHTML)
+  showElement(submitSucess)
 }
 
 
