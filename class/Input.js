1f1a87e47046d55d350d7bcb60017a469b403f6b
guillian <guillian.edo@gmail.com>
2025-09-03 10:04:07 +0200

    feat add class for input text, checkbox , radio , date and email

---
+++ class/Input.js
@@ -0,0 +1,83 @@
+class Input{
+    constructor({id,name,label,required = false,error = false,errorMessage}){
+        this.id = id
+        this.name = name
+        this.label = label
+        this.required = required
+        this.error = error
+        this.errorMessage = errorMessage
+    }
+}
+
+
+export class TextInput extends Input{
+    constructor({id,name,placeholder,label,minLength,maxLength,required,error,errorMessage}){
+        super({id,name,label,required,error,errorMessage})
+        this.type = "text"
+        this.minLength = minLength
+        this.maxLength = maxLength
+        this.placeholder = placeholder
+    }
+
+    render(){
+        return `
+            <div>
+                <label for="${this.id}">${this.label}</label>
+                <input type="${this.type}" id="${this.id}" name="${this.name}" placeholder="${this.placeholder}" minlength="${this.minLength}" maxlength="${this.maxLength}" required="${this.required}">
+                <span class="error">${this.error ? this.errorMessage : ''}</span>
+            </div>
+        `;
+    }
+}
+
+export class CheckboxInput extends Input{
+    constructor({id,name,label,required,error,errorMessage}){
+        super({id,name,label,required,error,errorMessage})
+        this.type = "checkbox"
+    }
+    render(){
+        return `
+            <div>
+                <label for="${this.id}">${this.label}</label>
+                <input type="${this.type}" id="${this.id}" name="${this.name}" required="${this.required}">
+                <span class="error">${this.error ? this.errorMessage : ''}</span>
+            </div>
+        `;
+    }
+}
+
+export class RadioInput extends Input{
+    constructor({id,name,label,required,error,errorMessage,options}){
+        super({id,name,label,required,error,errorMessage})
+        this.type = "radio"
+        this.options = options
+    }
+    render(){
+        return `
+            <div>
+                <label>${this.label}</label>
+                ${this.options.map(option => `
+                    <div>
+                        <input type="${this.type}" id="${this.id}_${option.value}" name="${this.name}" value="${option.value}" required="${this.required}">
+                        <label for="${this.id}_${option.value}">${option.label}</label>
+                    </div>
+                `).join('')}
+                <span class="error">${this.error ? this.errorMessage : ''}</span>
+            </div>
+        `;
+    }
+}
+
+export class DateInput extends Input{
+    constructor({id,name,label,required,error,errorMessage}){
+        super({id,name,label,required,error,errorMessage})
+        this.type = "date"
+    }
+}
+
+export class EmailInput extends Input{
+    constructor({id,name,label,required,error,errorMessage}){
+        super({id,name,label,required,error,errorMessage })
+        this.type = "email"
+    }
+}
