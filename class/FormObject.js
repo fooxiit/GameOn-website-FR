7c45ee09c07b365c4f9b16751218df584d6495fb
guillian <guillian.edo@gmail.com>
2025-09-03 12:30:20 +0200

    feat: implement abort controller for field change handling

--- class/FormObject.js
+++ class/FormObject.js
@@ -4,6 +4,7 @@ export class formObject{
        this.submitCallback = submitCallback
        this.submitButtonText = submitButtonText
        this.onRender = onRender
+       this.abortController = new AbortController();
     }
 
    
@@ -16,25 +17,46 @@ export class formObject{
         return values;
     }
 
-    submit(){
+    submit(e){
+        console.log("Form submitted");
+        e.preventDefault();
         Promise.all(this.fields.map(field => field.validate()))
             .then(() => {
                 this.submitCallback(this.getValues());
             })
             .catch(error => {
                 console.error("Validation failed:", error);
-                this.render()
+                //this.render()
             });
     }
 
     render(){
+        this.abortController.abort();
         const formHTML = `
-            <form onSubmit="(e)=>{this.onSubmit(e)}">
+            <form onSubmit="(e)=>{this.submit(e)}">
                 ${this.fields.map(field => field.render()).join('')}
                 <button type="submit">${this.submitButtonText}</button>
             </form>
         `;
         this.onRender(formHTML);
+        this.fields.forEach(field => {
+            console.log(field.id);
+            if(field.type === "radio"){
+                const radioDiv = document.querySelector(`#${field.id}`);
+                console.log(radioDiv);
+                radioDiv.querySelectorAll('input').forEach(radio => {
+                    radio.addEventListener('change', {signal: this.abortController.signal}, (e) => {
+                        field.onChange(e);
+                    });
+                });
+                
+            }else{
+                const fieldHtml = document.querySelector(`#${field.id}`);
+                fieldHtml.addEventListener('change', {signal: this.abortController.signal}, (e) => {
+                    field.onChange(e);
+                });
+            }
+        });
     }
 
 }
